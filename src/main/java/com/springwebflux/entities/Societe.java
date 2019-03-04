5a40cb27a7658d2d4a86ef93a53768c5d6597654
Ndiaye <moussa.ndiaye@gsietechnology.com>
2019-03-04 09:57:18 +0000

    first commit

---
+++ src/main/java/com/springwebflux/entities/Societe.java
@@ -0,0 +1,10 @@
+package com.springwebflux.entities;
+
+@Document
+@Data
+@
+public class Societe {
+    private String id ;
+    private  String name ;
+    private double prix ;
+}
---
+++ src/main/java/com/springwebflux/entities/Transaction.java
@@ -0,0 +1,4 @@
+package com.springwebflux.entities;
+
+public class Transaction {
+}
